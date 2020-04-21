83eb96a71f74f22d2440a6b1c58a229ceea42c3d
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2020-04-21 16:00:43 -0700

    set xterm terminus font name and sizes, fix ctrl-mouse menu size

--- bin/worktimer30min.fish
+++ bin/worktimer30min.fish
@@ -5,7 +5,7 @@ set break_time 5
 set break_time_sec (math "60*$break_time")
 
 set work_start_time 0
-set break_start_time 0
+set break_start_time (date +%s)
 set break_end_time 0
 set locked_time 0
 set unlocked_time 0
@@ -17,28 +17,31 @@ xscreensaver-command -watch | while read -l event
   if string match -q -r "^UNBLANK.*\$" $event
     set unlocked_time (date +%s)
     set locked_duration (math "($unlocked_time-$locked_time)/60")
+    echo
     echo "Screen was locked for $locked_duration min."
 
-    if test $unlocked_time -ge $break_start_time
-        if test $unlocked_time -ge $break_end_time
-            # break done, start new work time
+    if test $unlocked_time -ge $break_start_time # if the break has started
+        if test $unlocked_time -ge $break_end_time # if break is over
+            # break done, start new work time cycle
             set work_start_time (date +%s)
             set break_start_time (math $work_start_time+$work_time_sec)
             set break_end_time (math $break_start_time+$break_time_sec)
-            echo "Break over!"
-            echo "Work time left: $work_time min"
-            echo "Then break for: $break_time min"
+            echo "================================"
+            date
+            echo "Break complete!"
+            echo "Work for $work_time min, then break for $break_time min."
             sleep $work_time_sec
             xscreensaver-command -lock
-        else if test $unlocked_time -lt $break_end_time
+        else if test $unlocked_time -lt $break_end_time # if break is not over yet
             set min_till_break_done (math "($break_end_time-$unlocked_time)/60")
             echo "Break time left: $min_till_break_done"
+            # relock the screen until break is over
             sleep 5
             xscreensaver-command -lock
         else
             echo "shouldn't get here?"
         end
-    else
+    else # work time still going (no break yet)
         set min_till_break_start (math "($break_start_time-$unlocked_time)/60")
         echo "Work time left: $min_till_break_start"
         sleep (math "60*$min_till_break_start")
