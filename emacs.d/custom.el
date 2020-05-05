6377b8a1205f5986afc9ce65780b1c211faf0d1e
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2020-05-05 11:57:42 -0700

    [emacs]
    - start server if not on the command line
    - interrupt shell alias
    - move airline custom to setq

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -7,20 +7,20 @@
  '(elfeed-feeds
    (quote
     ("https://hackaday.com/blog/feed/" "http://planet.emacsen.org/atom.xml")))
- '(elscreen-display-tab nil)
  '(inhibit-startup-screen nil)
  '(mac-mouse-wheel-smooth-scroll nil)
  '(magit-log-arguments (quote ("--graph" "--color" "--decorate")))
  '(magit-status-buffer-switch-function (quote switch-to-buffer))
  '(package-selected-packages
    (quote
-    (ibuffer-projectile highlight-indent-guides major-mode-hydra airline-themes hide-mode-line modus-operandi-theme modus-vivendi-theme markdown-mode+ markdown-mode yasnippet gif-screencast lsp-mode gn-mode ninja-mode gnuplot ox-gfm fennel-mode blimp use-package ob-async ivy-xref dts-mode forth-mode company-jedi jedi-core jinja2-mode mmm-jinja2 request fill-column-indicator sqlup-mode scad-mode scad-preview toc-org steam ssh-config-mode ssh-agency general ht paradox sql-indent pylint moonscript smex xterm-color fish-mode vimrc-mode company-shell vlf exec-path-from-shell edit-server evil-visualstar json-mode expand-region yari yaml-mode wgrep-pt wgrep-ag subatomic256-theme scss-mode robe rainbow-delimiters pt pandoc-mode js2-mode jedi htmlize haml-mode go-mode feature-mode evil-commentary esup emmet-mode edbi company-inf-ruby coffee-mode ag ace-link)))
+    (ibuffer-projectile highlight-indent-guides major-mode-hydra airline-themes hide-mode-line modus-operandi-theme modus-vivendi-theme markdown-mode+ markdown-mode yasnippet gif-screencast lsp-mode gn-mode ninja-mode gnuplot ox-gfm fennel-mode blimp use-package ob-async ivy-xref dts-mode forth-mode company-jedi jedi-core jinja2-mode mmm-jinja2 request fill-column-indicator sqlup-mode scad-mode scad-preview toc-org steam ssh-config-mode ssh-agency general ht paradox sql-indent pylint moonscript smex xterm-color fish-mode vimrc-mode company-shell vlf exec-path-from-shell edit-server json-mode expand-region yari yaml-mode wgrep-pt wgrep-ag subatomic256-theme scss-mode robe rainbow-delimiters pt pandoc-mode js2-mode jedi htmlize haml-mode go-mode feature-mode evil-commentary esup emmet-mode edbi company-inf-ruby coffee-mode ag ace-link)))
  '(paradox-github-token t)
  '(projectile-mode-line (quote (:eval (format " p[%s]" (projectile-project-name)))))
  '(python-indent-guess-indent-offset t)
  '(safe-local-variable-values
    (quote
-    ((org-html-toplevel-hlevel . 1)
+    ((org-html-table-default-attributes :class "mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp" :border "1" :cellspacing "0" :cellpadding "0" :rules "all" :frame "border")
+     (org-html-toplevel-hlevel . 1)
      (org-html-table-default-attributes :border "1" :cellspacing "0" :cellpadding "0" :rules "all" :frame "border")
      (org-html-table-data-tags "<td class=\"mdl-data-table__cell--non-numeric\" %s>" . "</td>")
      (org-html-table-header-tags "<th class=\"mdl-data-table__cell--non-numeric\" scope=\"%s\"%s>" . "</th>")
