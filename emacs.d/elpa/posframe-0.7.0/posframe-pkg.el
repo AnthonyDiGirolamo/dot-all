e680b6e169ba996a6112bfac8a8ba10fdc275cc8
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2020-05-13 09:47:36 -0700

    try out posframe for ivy and hydra

---
+++ emacs.d/elpa/posframe-0.7.0/posframe-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "posframe" "0.7.0" "Pop a posframe (just a frame) at point" '((emacs "26")) :keywords '("convenience" "tooltip") :authors '(("Feng Shu" . "tumashu@163.com")) :maintainer '("Feng Shu" . "tumashu@163.com") :url "https://github.com/tumashu/posframe")
