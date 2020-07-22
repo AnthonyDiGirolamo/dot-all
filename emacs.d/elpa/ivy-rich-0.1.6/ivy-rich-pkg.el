0bda93a49eba71b187bdf6ba66c056194cc12d40
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2020-07-22 10:50:27 -0700

    [emacs] install ivy-rich for nicer ivy-switch-buffer

---
+++ emacs.d/elpa/ivy-rich-0.1.6/ivy-rich-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ivy-rich" "0.1.6" "More friendly display transformer for ivy." '((emacs "24.5") (ivy "0.8.0")) :commit "840e13314774a40b69f10f0a15ce1d6af4187b12" :keywords '("ivy") :authors '(("Yevgnen Koh" . "wherejoystarts@gmail.com")) :maintainer '("Yevgnen Koh" . "wherejoystarts@gmail.com"))
