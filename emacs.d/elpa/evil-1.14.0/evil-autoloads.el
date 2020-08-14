d285a49366bcdae2091f5961dd88378df2c1feda
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2020-08-14 12:36:51 -0700

    [emacs] evil-ex-search-* wasn't working due to recenter advice

--- emacs.d/elpa/evil-1.14.0/evil-autoloads.el
+++ emacs.d/elpa/evil-1.14.0/evil-autoloads.el
@@ -6,116 +6,286 @@
                          (or (file-name-directory #$) (car load-path))))
 
 
-;;;### (autoloads nil "evil-command-window" "evil-command-window.el"
-;;;;;;  (0 0 0 0))
+;;;### (autoloads "actual autoloads are elsewhere" "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-command-window"
+;;;;;;  "evil-command-window.el" (0 0 0 0))
 ;;; Generated autoloads from evil-command-window.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-command-window" '("evil-")))
+
+;;;***
+
+;;;### (autoloads "actual autoloads are elsewhere" "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-commands"
+;;;;;;  "evil-commands.el" (0 0 0 0))
+;;; Generated autoloads from evil-commands.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-commands" '("evil-")))
+
+;;;***
+
+;;;### (autoloads "actual autoloads are elsewhere" "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-common"
+;;;;;;  "evil-common.el" (0 0 0 0))
+;;; Generated autoloads from evil-common.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-common" '("bounds-of-evil-" "evil-" "forward-evil-")))
+
+;;;***
+
+;;;### (autoloads nil "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-core"
+;;;;;;  "evil-core.el" "ed7f254f900c7dfe1de5bc51ac8c6b47")
+;;; Generated autoloads from evil-core.el
+ (autoload 'evil-mode "evil" nil t)
+
+;;;### (autoloads "actual autoloads are elsewhere" "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-core"
+;;;;;;  "evil-core.el" (0 0 0 0))
+;;; Generated autoloads from evil-core.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-core" '("evil-" "turn-o")))
+
+;;;***
+
+;;;***
+
+;;;### (autoloads "actual autoloads are elsewhere" "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-digraphs"
+;;;;;;  "evil-digraphs.el" (0 0 0 0))
+;;; Generated autoloads from evil-digraphs.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-digraphs" '("evil-digraph")))
+
+;;;***
+
+;;;### (autoloads "actual autoloads are elsewhere" "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-ex"
+;;;;;;  "evil-ex.el" (0 0 0 0))
+;;; Generated autoloads from evil-ex.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-ex" '("evil-")))
+
+;;;***
+
+;;;### (autoloads "actual autoloads are elsewhere" "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-integration"
+;;;;;;  "evil-integration.el" (0 0 0 0))
+;;; Generated autoloads from evil-integration.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-integration" '("evil-")))
+
+;;;***
+
+;;;### (autoloads "actual autoloads are elsewhere" "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-jumps"
+;;;;;;  "evil-jumps.el" (0 0 0 0))
+;;; Generated autoloads from evil-jumps.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-jumps" '("evil-")))
+
+;;;***
+
+;;;### (autoloads "actual autoloads are elsewhere" "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-macros"
+;;;;;;  "evil-macros.el" (0 0 0 0))
+;;; Generated autoloads from evil-macros.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-macros" '("evil-")))
+
+;;;***
+
+;;;### (autoloads "actual autoloads are elsewhere" "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-maps"
+;;;;;;  "evil-maps.el" (0 0 0 0))
+;;; Generated autoloads from evil-maps.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-maps" '("evil-")))
+
+;;;***
+
+;;;### (autoloads "actual autoloads are elsewhere" "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-repeat"
+;;;;;;  "evil-repeat.el" (0 0 0 0))
+;;; Generated autoloads from evil-repeat.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-repeat" '("evil-")))
+
+;;;***
+
+;;;### (autoloads "actual autoloads are elsewhere" "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-search"
+;;;;;;  "evil-search.el" (0 0 0 0))
+;;; Generated autoloads from evil-search.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-search" '("evil-")))
+
+;;;***
+
+;;;### (autoloads "actual autoloads are elsewhere" "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-states"
+;;;;;;  "evil-states.el" (0 0 0 0))
+;;; Generated autoloads from evil-states.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-states" '("evil-")))
+
+;;;***
+
+;;;### (autoloads "actual autoloads are elsewhere" "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-types"
+;;;;;;  "evil-types.el" (0 0 0 0))
+;;; Generated autoloads from evil-types.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-types" '("evil-ex-get-optional-register-and-count")))
+
+;;;***
+
+;;;### (autoloads "actual autoloads are elsewhere" "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-vars"
+;;;;;;  "evil-vars.el" (0 0 0 0))
+;;; Generated autoloads from evil-vars.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "../../../dot-all/emacs.d/elpa/evil-1.14.0/evil-vars" '("evil-")))
+
+;;;***
+
+;;;### (autoloads "actual autoloads are elsewhere" "evil-command-window"
+;;;;;;  "../../../../.emacs.d/elpa/evil-1.14.0/evil-command-window.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../.emacs.d/elpa/evil-1.14.0/evil-command-window.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-command-window" '("evil-")))
 
 ;;;***
 
-;;;### (autoloads nil "evil-commands" "evil-commands.el" (0 0 0 0))
-;;; Generated autoloads from evil-commands.el
+;;;### (autoloads "actual autoloads are elsewhere" "evil-commands"
+;;;;;;  "../../../../.emacs.d/elpa/evil-1.14.0/evil-commands.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../.emacs.d/elpa/evil-1.14.0/evil-commands.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-commands" '("evil-")))
 
 ;;;***
 
-;;;### (autoloads nil "evil-common" "evil-common.el" (0 0 0 0))
-;;; Generated autoloads from evil-common.el
+;;;### (autoloads "actual autoloads are elsewhere" "evil-common"
+;;;;;;  "../../../../.emacs.d/elpa/evil-1.14.0/evil-common.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from ../../../../.emacs.d/elpa/evil-1.14.0/evil-common.el
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-common" '("evil-" "forward-evil-" "bounds-of-evil-")))
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-common" '("bounds-of-evil-" "evil-" "forward-evil-")))
 
 ;;;***
 
-;;;### (autoloads nil "evil-core" "evil-core.el" (0 0 0 0))
-;;; Generated autoloads from evil-core.el
+;;;### (autoloads nil "evil-core" "../../../../.emacs.d/elpa/evil-1.14.0/evil-core.el"
+;;;;;;  "ed7f254f900c7dfe1de5bc51ac8c6b47")
+;;; Generated autoloads from ../../../../.emacs.d/elpa/evil-1.14.0/evil-core.el
  (autoload 'evil-mode "evil" nil t)
+
+;;;### (autoloads "actual autoloads are elsewhere" "evil-core" "../../../../.emacs.d/elpa/evil-1.14.0/evil-core.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../.emacs.d/elpa/evil-1.14.0/evil-core.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-core" '("evil-" "turn-o")))
 
 ;;;***
+
+;;;***
 
-;;;### (autoloads nil "evil-digraphs" "evil-digraphs.el" (0 0 0 0))
-;;; Generated autoloads from evil-digraphs.el
+;;;### (autoloads "actual autoloads are elsewhere" "evil-digraphs"
+;;;;;;  "../../../../.emacs.d/elpa/evil-1.14.0/evil-digraphs.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../.emacs.d/elpa/evil-1.14.0/evil-digraphs.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-digraphs" '("evil-digraph")))
 
 ;;;***
 
-;;;### (autoloads nil "evil-ex" "evil-ex.el" (0 0 0 0))
-;;; Generated autoloads from evil-ex.el
+;;;### (autoloads "actual autoloads are elsewhere" "evil-ex" "../../../../.emacs.d/elpa/evil-1.14.0/evil-ex.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../.emacs.d/elpa/evil-1.14.0/evil-ex.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-ex" '("evil-")))
 
 ;;;***
 
-;;;### (autoloads nil "evil-integration" "evil-integration.el" (0
-;;;;;;  0 0 0))
-;;; Generated autoloads from evil-integration.el
+;;;### (autoloads "actual autoloads are elsewhere" "evil-integration"
+;;;;;;  "../../../../.emacs.d/elpa/evil-1.14.0/evil-integration.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../.emacs.d/elpa/evil-1.14.0/evil-integration.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-integration" '("evil-")))
 
 ;;;***
 
-;;;### (autoloads nil "evil-jumps" "evil-jumps.el" (0 0 0 0))
-;;; Generated autoloads from evil-jumps.el
+;;;### (autoloads "actual autoloads are elsewhere" "evil-jumps" "../../../../.emacs.d/elpa/evil-1.14.0/evil-jumps.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../.emacs.d/elpa/evil-1.14.0/evil-jumps.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-jumps" '("evil-")))
 
 ;;;***
 
-;;;### (autoloads nil "evil-macros" "evil-macros.el" (0 0 0 0))
-;;; Generated autoloads from evil-macros.el
+;;;### (autoloads "actual autoloads are elsewhere" "evil-macros"
+;;;;;;  "../../../../.emacs.d/elpa/evil-1.14.0/evil-macros.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from ../../../../.emacs.d/elpa/evil-1.14.0/evil-macros.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-macros" '("evil-")))
 
 ;;;***
 
-;;;### (autoloads nil "evil-maps" "evil-maps.el" (0 0 0 0))
-;;; Generated autoloads from evil-maps.el
+;;;### (autoloads "actual autoloads are elsewhere" "evil-maps" "../../../../.emacs.d/elpa/evil-1.14.0/evil-maps.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../.emacs.d/elpa/evil-1.14.0/evil-maps.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-maps" '("evil-")))
 
 ;;;***
 
-;;;### (autoloads nil "evil-repeat" "evil-repeat.el" (0 0 0 0))
-;;; Generated autoloads from evil-repeat.el
+;;;### (autoloads "actual autoloads are elsewhere" "evil-repeat"
+;;;;;;  "../../../../.emacs.d/elpa/evil-1.14.0/evil-repeat.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from ../../../../.emacs.d/elpa/evil-1.14.0/evil-repeat.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-repeat" '("evil-")))
 
 ;;;***
 
-;;;### (autoloads nil "evil-search" "evil-search.el" (0 0 0 0))
-;;; Generated autoloads from evil-search.el
+;;;### (autoloads "actual autoloads are elsewhere" "evil-search"
+;;;;;;  "../../../../.emacs.d/elpa/evil-1.14.0/evil-search.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from ../../../../.emacs.d/elpa/evil-1.14.0/evil-search.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-search" '("evil-")))
 
 ;;;***
 
-;;;### (autoloads nil "evil-states" "evil-states.el" (0 0 0 0))
-;;; Generated autoloads from evil-states.el
+;;;### (autoloads "actual autoloads are elsewhere" "evil-states"
+;;;;;;  "../../../../.emacs.d/elpa/evil-1.14.0/evil-states.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from ../../../../.emacs.d/elpa/evil-1.14.0/evil-states.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-states" '("evil-")))
 
 ;;;***
 
-;;;### (autoloads nil "evil-types" "evil-types.el" (0 0 0 0))
-;;; Generated autoloads from evil-types.el
+;;;### (autoloads "actual autoloads are elsewhere" "evil-types" "../../../../.emacs.d/elpa/evil-1.14.0/evil-types.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../.emacs.d/elpa/evil-1.14.0/evil-types.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-types" '("evil-ex-get-optional-register-and-count")))
 
 ;;;***
 
-;;;### (autoloads nil "evil-vars" "evil-vars.el" (0 0 0 0))
-;;; Generated autoloads from evil-vars.el
+;;;### (autoloads "actual autoloads are elsewhere" "evil-vars" "../../../../.emacs.d/elpa/evil-1.14.0/evil-vars.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../.emacs.d/elpa/evil-1.14.0/evil-vars.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-vars" '("evil-")))
 
 ;;;***
 
-;;;### (autoloads nil nil ("evil-development.el" "evil-keybindings.el"
-;;;;;;  "evil-pkg.el" "evil.el") (0 0 0 0))
+;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/evil-1.14.0/evil-autoloads.el"
+;;;;;;  "../../../../.emacs.d/elpa/evil-1.14.0/evil-command-window.el"
+;;;;;;  "../../../../.emacs.d/elpa/evil-1.14.0/evil-commands.el"
+;;;;;;  "../../../../.emacs.d/elpa/evil-1.14.0/evil-common.el" "../../../../.emacs.d/elpa/evil-1.14.0/evil-core.el"
+;;;;;;  "../../../../.emacs.d/elpa/evil-1.14.0/evil-development.el"
+;;;;;;  "../../../../.emacs.d/elpa/evil-1.14.0/evil-digraphs.el"
+;;;;;;  "../../../../.emacs.d/elpa/evil-1.14.0/evil-ex.el" "../../../../.emacs.d/elpa/evil-1.14.0/evil-integration.el"
+;;;;;;  "../../../../.emacs.d/elpa/evil-1.14.0/evil-jumps.el" "../../../../.emacs.d/elpa/evil-1.14.0/evil-keybindings.el"
+;;;;;;  "../../../../.emacs.d/elpa/evil-1.14.0/evil-macros.el" "../../../../.emacs.d/elpa/evil-1.14.0/evil-maps.el"
+;;;;;;  "../../../../.emacs.d/elpa/evil-1.14.0/evil-pkg.el" "../../../../.emacs.d/elpa/evil-1.14.0/evil-repeat.el"
+;;;;;;  "../../../../.emacs.d/elpa/evil-1.14.0/evil-search.el" "../../../../.emacs.d/elpa/evil-1.14.0/evil-states.el"
+;;;;;;  "../../../../.emacs.d/elpa/evil-1.14.0/evil-types.el" "../../../../.emacs.d/elpa/evil-1.14.0/evil-vars.el"
+;;;;;;  "../../../../.emacs.d/elpa/evil-1.14.0/evil.el" "evil-command-window.el"
+;;;;;;  "evil-commands.el" "evil-common.el" "evil-core.el" "evil-digraphs.el"
+;;;;;;  "evil-ex.el" "evil-integration.el" "evil-jumps.el" "evil-macros.el"
+;;;;;;  "evil-maps.el" "evil-repeat.el" "evil-search.el" "evil-states.el"
+;;;;;;  "evil-types.el" "evil-vars.el") (0 0 0 0))
 
 ;;;***
 
--- emacs.d/elpa/evil-1.14.0/evil-pkg.el
+++ emacs.d/elpa/evil-1.14.0/evil-pkg.el
@@ -2,7 +2,8 @@
   '((emacs "24.1")
     (undo-tree "0.6.3")
     (goto-chg "1.6")
-    (cl-lib "0.5")))
+    (cl-lib "0.5"))
+  :commit "4dc63903d9688e2ce838a220b0e24d8f14a64c12")
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:
