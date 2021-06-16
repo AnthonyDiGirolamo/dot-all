348b03d5d99f9675e5d95f82a6bd8fdf07f7bcdb
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2021-06-16 09:08:58 -0700

    [emacs] elc recompile script fix, adjust flycheck column widths

--- emacs.d/recompile-elpa-elc-files.sh
+++ emacs.d/recompile-elpa-elc-files.sh
@@ -2,7 +2,7 @@
 rm -f ~/.emacs.d/cache/autoload*
 rm -f ~/.emacs.d/package-quickstart.el
 find ~/.emacs.d/elpa/ -iname "*.elc" -delete
-find ./.emacs.d/elpa/ -maxdepth 1 -type d -empty -delete
+find ~/.emacs.d/elpa/ -maxdepth 1 -type d -empty -delete
 emacs -Q --batch --eval '(byte-recompile-directory "~/.emacs.d/elpa/" 0 t)'
 time emacs -nw --eval '(progn (package-quickstart-refresh) (save-buffers-kill-terminal))'
 time emacs -nw --eval '(save-buffers-kill-terminal)'
