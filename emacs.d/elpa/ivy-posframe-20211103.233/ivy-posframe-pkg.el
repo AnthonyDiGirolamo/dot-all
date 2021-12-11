f8cd5e573da87c6a95d08fbbfbec08dc50b82140
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2021-12-11 09:10:34 -0800

    [emacs] upgrade ivy and friends

---
+++ emacs.d/elpa/ivy-posframe-20211103.233/ivy-posframe-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from ivy-posframe.el  -*- no-byte-compile: t -*-
+(define-package "ivy-posframe" "20211103.233" "Using posframe to show Ivy" '((emacs "26.0") (posframe "1.0.0") (ivy "0.13.0")) :commit "5d9420252ca855d6d206f1f8ef5993a6be3c618f" :authors '(("Feng Shu" . "tumashu@163.com") ("Naoya Yamashita" . "conao3@gmail.com")) :maintainer '("Feng Shu" . "tumashu@163.com") :keywords '("abbrev" "convenience" "matching" "ivy") :url "https://github.com/tumashu/ivy-posframe")
--- emacs.d/elpa/posframe-0.7.0/posframe-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "posframe" "0.7.0" "Pop a posframe (just a frame) at point" '((emacs "26")) :keywords '("convenience" "tooltip") :authors '(("Feng Shu" . "tumashu@163.com")) :maintainer '("Feng Shu" . "tumashu@163.com") :url "https://github.com/tumashu/posframe")
