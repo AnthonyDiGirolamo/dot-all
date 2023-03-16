3fd8fb2191df13146d6778bf7ad3198bf9ff17a7
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2023-03-16 13:14:06 -0700

    [emacs] upgrade modus-themes

--- emacs.d/elpa/modus-themes-20230313.831/modus-themes-pkg.el
+++ emacs.d/elpa/modus-themes-20230313.831/modus-themes-pkg.el
@@ -1,6 +1,6 @@
-(define-package "modus-themes" "20221101.424" "Elegant, highly legible and customizable themes"
+(define-package "modus-themes" "20230313.831" "Elegant, highly legible and customizable themes"
   '((emacs "27.1"))
-  :commit "f1149c18005d31638b3701f9a89819b133b4360e" :authors
+  :commit "df3e2a80de11449250034af1d5a31cdca64c6e8a" :authors
   '(("Protesilaos Stavrou" . "info@protesilaos.com"))
   :maintainer
   '("Modus-Themes Development" . "~protesilaos/modus-themes@lists.sr.ht")
