142dd67aeb6877ffa44b32975885d8d92695dd44
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2023-03-16 14:54:18 -0700

    [emacs] upgrade flycheck, rg with extra args, undo-tree lint fix

--- emacs.d/elpa/flycheck-20230306.414/flycheck-pkg.el
+++ emacs.d/elpa/flycheck-20230306.414/flycheck-pkg.el
@@ -1,15 +1,15 @@
-(define-package "flycheck" "20200323.243" "On-the-fly syntax checking"
-  '((dash "2.12.1")
+(define-package "flycheck" "20230306.414" "On-the-fly syntax checking"
+  '((emacs "25.1")
+    (dash "2.12.1")
     (pkg-info "0.4")
     (let-alist "1.0.4")
-    (seq "1.11")
-    (emacs "24.3"))
-  :keywords
-  '("convenience" "languages" "tools")
-  :authors
+    (seq "1.11"))
+  :commit "5f2ef177cb21ae8b73714575802beef04abd0f5e" :authors
   '(("Sebastian Wiesner" . "swiesner@lunaryorn.com"))
   :maintainer
   '("Clément Pit-Claudel" . "clement.pitclaudel@live.com")
+  :keywords
+  '("convenience" "languages" "tools")
   :url "http://www.flycheck.org")
 ;; Local Variables:
 ;; no-byte-compile: t
