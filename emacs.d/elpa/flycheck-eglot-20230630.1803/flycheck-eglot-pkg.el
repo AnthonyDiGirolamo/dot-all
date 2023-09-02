9caf5895f4c6210d44f07b4a5f4d86599e1999ee
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2023-09-02 12:07:26 -0700

    [emacs] upgrade lsp, flycheck, flycheck-eglot

---
+++ emacs.d/elpa/flycheck-eglot-20230630.1803/flycheck-eglot-pkg.el
@@ -0,0 +1,16 @@
+(define-package "flycheck-eglot" "20230630.1803" "Flycheck support for eglot"
+  '((emacs "28.1")
+    (eglot "1.9")
+    (flycheck "32"))
+  :commit "9ff8d0068be59b1450964b390349d75a68af21ed" :authors
+  '(("Sergey Firsov" . "intramurz@gmail.com"))
+  :maintainers
+  '(("Sergey Firsov" . "intramurz@gmail.com"))
+  :maintainer
+  '("Sergey Firsov" . "intramurz@gmail.com")
+  :keywords
+  '("convenience" "language" "tools")
+  :url "https://github.com/intramurz/flycheck-eglot")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs.d/elpa/lsp-mode-20230901.1743/lsp-mode-pkg.el
@@ -0,0 +1,21 @@
+(define-package "lsp-mode" "20230901.1743" "LSP mode"
+  '((emacs "27.1")
+    (dash "2.18.0")
+    (f "0.20.0")
+    (ht "2.3")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0")
+    (eldoc "1.11"))
+  :commit "271525a06ba7f61d1fdfe28c0520b1acd8063428" :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainers
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :keywords
+  '("languages")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
