fc72c654145a4c48b0811679774bb84175aa3427
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2024-06-02 11:29:14 -0700

    [emacs] Upgrade packages, use dashboard-mode

---
+++ emacs.d/elpa/flycheck-eglot-20240214.1937/flycheck-eglot-pkg.el
@@ -0,0 +1,16 @@
+(define-package "flycheck-eglot" "20240214.1937" "Flycheck support for eglot"
+  '((emacs "28.1")
+    (eglot "1.9")
+    (flycheck "32"))
+  :commit "a38101634c55ef6cfcfc74b4fd9a50b541b42457" :authors
+  '(("Sergey Firsov" . "intramurz@gmail.com"))
+  :maintainers
+  '(("Sergey Firsov" . "intramurz@gmail.com"))
+  :maintainer
+  '("Sergey Firsov" . "intramurz@gmail.com")
+  :keywords
+  '("convenience" "language" "tools")
+  :url "https://github.com/flycheck/flycheck-eglot")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs.d/elpa/ninja-mode-1.12.1/ninja-mode-pkg.el
@@ -0,0 +1,6 @@
+(define-package "ninja-mode" "1.12.1" "Major mode for editing .ninja files"
+  '((emacs "24"))
+  :commit "2daa09ba270b0a43e1929d29b073348aa985dfaa")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
