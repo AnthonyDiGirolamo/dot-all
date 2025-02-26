b1f25c36d84ac76dc0f2a4c42d45c306f6e402c0
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2025-02-26 15:27:25 -0800

    [emacs] upgrade yankpad

--- emacs.d/elpa/yankpad-20220201.2104/yankpad-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from yankpad.el  -*- no-byte-compile: t -*-
-(define-package "yankpad" "20220201.2104" "Paste snippets from an org-mode file" '((emacs "25.1")) :commit "927e6d26956ac7219b8a69d641acf486854fba16" :authors '(("Erik Sjöstrand")) :maintainer '("Erik Sjöstrand") :keywords '("abbrev" "convenience") :url "http://github.com/Kungsgeten/yankpad")
---
+++ emacs.d/elpa/yankpad-20240926.2114/yankpad-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "yankpad" "20240926.2114"
+  "Paste snippets from an org-mode file."
+  '((emacs "25.1"))
+  :url "http://github.com/Kungsgeten/yankpad"
+  :commit "85cfa3242abb64018eb944271460bd533588d9c1"
+  :revdesc "85cfa3242abb"
+  :keywords '("abbrev" "convenience"))
