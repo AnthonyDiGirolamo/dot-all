92621c8a2a854f8cde6763fa513ff54cf6739149
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2025-07-28 12:49:48 -0700

    [emacs] Update doom-themes

---
+++ emacs.d/elpa/doom-modeline-4.2.0/doom-modeline-pkg.el
@@ -0,0 +1,13 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "doom-modeline" "4.2.0"
+  "A minimal and modern mode-line."
+  '((emacs       "25.1")
+    (compat      "30.1.0.0")
+    (nerd-icons  "0.1.0")
+    (shrink-path "0.3.1"))
+  :url "https://github.com/seagle0128/doom-modeline"
+  :commit "14b09e0b20f0e210be54167b12c3b5a201eb0327"
+  :revdesc "14b09e0b20f0"
+  :keywords '("faces" "mode-line")
+  :authors '(("Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainers '(("Vincent Zhang" . "seagle0128@gmail.com")))
